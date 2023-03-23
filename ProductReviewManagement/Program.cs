46e14635d95314a4eae5bfe0551ebc3d1eec4f97
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-03-23 18:26:59 +0530

    [Ayushi] Welcoming you

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management Problem");
+            Console.ReadLine();
+        }
+    }
+}
